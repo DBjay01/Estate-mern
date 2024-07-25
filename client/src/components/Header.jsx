af64c3c16998bae1f437174ed6f6de2eb04b8eb8
Dhananjay <dhananjayvbile10@gmail.com>
2024-07-25 12:23:44 +0530

    create and run server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -32,7 +32,7 @@ export default function Header() {
           </Link>
 
           <Link to='/About'>
-            <li className='font-bold text-sm sm:text-lg  hidden sm:inline bg-gradient-to-r from-violet-600 to-indigo-600 bg-clip-text text-transparent hidden sm:inline  '>
+            <li className='font-bold text-sm sm:text-lg  hidden sm:inline bg-gradient-to-r from-violet-600 to-indigo-600 bg-clip-text text-transparent  '>
               About
             </li>
           </Link>
